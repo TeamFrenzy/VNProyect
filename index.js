952cec1815ac2701467d2015d586858074cf121c
Gabriel Funes <gabrielffunes@hotmail.com>
2020-04-20 20:35:37 -0300

    never

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const htmlPath = path.join(__dirname, 'gameWeb2');
+const htmlPath = path.join(__dirname, 'gameWeb4');
 const logger = require('morgan');
 //const cors = require('cors');
 const bodyParser = require('body-parser');
